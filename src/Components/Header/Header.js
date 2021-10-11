257cb1599871d62ea0128eef7c6b8e0393a0ad0d
abdullaibnhanif <abdulla.mamun100@gmail.com>
2021-10-11 19:43:00 +0600

    58-2

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Header.css";
 
 const Header = () => {
     return (
-        <div>
-
+        <div className="header">
+            <Link to="/home">Home</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+            <button>Sign Out</button>
         </div>
     );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-
+            <h2>This is Home</h2>
         </div>
     );
 };
